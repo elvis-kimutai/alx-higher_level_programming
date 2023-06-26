e3891b1cbe61c4553d646a95f8fa6ee71db73195
elvis-kimutai <cheruiyotkim17@gmail.com>
2023-06-26 13:29:42 -0700

    does exactly the same as sh*t as  bytecode

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# Does exactly the same, sh*t as the python bytecode given
+
+
+def magic_calculation(a, b):
+    total = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception('Too far')
+            else:
+                total += (a ** b) / i
+        except Exception:
+            total = b + a
+            break
+    return (total)
