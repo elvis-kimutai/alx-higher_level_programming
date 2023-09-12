c908f6d6138709d1c4cda91770a466abc985f2f7
elvis-kimutai <cheruiyotkim17@gmail.com>
2023-09-11 17:05:08 -0700

    line arrray

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const line_array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let i = 0; i < line_array.length; i++) {
-  console.log(line_array[i]);
+const linearray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < linearray.length; i++) {
+  console.log(linearray[i]);
 }
