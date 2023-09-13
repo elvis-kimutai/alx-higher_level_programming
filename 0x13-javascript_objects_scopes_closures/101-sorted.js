3ff30e30eba2432066ac573f19cd85fbc0029f97
elvis-kimutai <cheruiyotkim17@gmail.com>
2023-09-12 17:45:54 -0700

    script that imports a dictionary of occurrences by user id

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 
 const dict = require('./101-data').dict;
-const newd = {};
-
-for (const key in dict) {
-  if (newd[dict[key]] === undefined) {
-    newd[dict[key]] = (key);
-  } else {
-    newd[dict[key]].push(key);
+const reversedDict = {};
+for (const userId in dict) {
+  const occurrence = dict[userId];
+  if (!reversedDict[occurrence]) {
+    reversedDict[occurrence] = [];
   }
+  reversedDict[occurrence].push(userId);
 }
-console.log(newd);
+
+console.log(reversedDict);
