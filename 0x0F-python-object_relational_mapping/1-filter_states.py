741d7a4e71bf3d7f712601a511a7c025e8dfd523
elvis-kimutai <cheruiyotkim17@gmail.com>
2023-09-17 07:45:33 -0700

    script that lists all states with a name starting with N (upper N)

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" module that lists all states starting with N from the database """
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+
+    # connects to the database
+    the_db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                             passwd=argv[2], the_db=argv[3])
+
+    the_cursor = the_db.cursor()
+    the_cursor.execute("SELECT * FROM states")
+
+    thedata = cur.fetchall()
+    for rows in thedata:
+        print(i)
+    # Closes up process
+    the_cursor.close()
+    the_db.close()
