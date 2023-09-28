f059c226ba74dec9cb130df8514b9cc19535d8a2
elvis-kimutai <cheruiyotkim17@gmail.com>
2023-09-28 15:41:02 -0700

    . cURL to the end

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-#Bash scrip that takes in url and sends GET request to the
-#URL & display body of response
+#Bash scrip that takes in url and sends GET request to the URL & display body of response
 curl -sL "$1"
