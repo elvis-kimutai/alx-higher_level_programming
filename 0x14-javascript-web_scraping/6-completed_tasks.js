7743f41482ab4818d00d85f5db0f9593e9d3da49
elvis-kimutai <cheruiyotkim17@gmail.com>
2023-10-24 06:56:24 -0700

    completed tasks

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+// import the module
+const request = require('request');
+const apiUrl = process.argv[2];
+const dictionary = {};
+
+request(apiUrl, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  } else {
+    const data = JSON.parse(body);
+    data.forEach(function (result) {
+      if (result.completed === true) {
+        const userid = result.userId;
+        if (!(userid in dictionary)) {
+          dictionary[userid] = 0;
+        }
+        dictionary[userid] += 1;
+      }
+    });
+    console.log(dictionary);
+  }
+});
